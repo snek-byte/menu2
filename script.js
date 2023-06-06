8aa0615bde2aeee07b4a246fe7af9545484708e8
sweston <sarahwestonak@gmail.com>
2023-06-06 17:58:39 -0600

    script.js

--- script.js
+++ script.js
@@ -1,58 +1,59 @@
-// Get elements from DOM
-const menuBtn = document.querySelector("#menu-btn");
-const closeBtn = document.querySelector("#close-btn");
+// Obtain elements from DOM
+const menuButton = document.querySelector("#menu-btn");
+const closeButton = document.querySelector("#close-btn");
 const menuOverlay = document.querySelector("#menu-overlay");
 const navItems = Array.from(document.querySelectorAll(".menu-item"));
 const submenus = Array.from(document.querySelectorAll(".submenu"));
 
-// Create a Map to store the visibility state of each submenu
-const submenuVisible = new Map(submenus.map(submenu => [submenu, false]));
+// Initialize a Map to save visibility status of each submenu
+let submenuVisibility = new Map(submenus.map(submenu => [submenu, false]));
 
 // Function to open the menu
 function openMenu() {
   menuOverlay.style.display = "block";
-  gsap.fromTo(
-    navItems,
-    { x: "-100%", autoAlpha: 0 },
-    { x: "0", duration: 0.5, stagger: 0.2, autoAlpha: 1 }
-  );
+  closeButton.style.display = "block"; // Display close button when menu is open
+
+  // Animation
+  gsap.fromTo(navItems, { x: "-100%", autoAlpha: 0 }, { x: "0", duration: 0.5, stagger: 0.2, autoAlpha: 1 });
 }
 
 // Function to close the menu
 function closeMenu() {
   gsap.to(navItems, { x: "-100%", duration: 0.5, autoAlpha: 0 }).then(() => {
     menuOverlay.style.display = "none";
+    closeButton.style.display = "none"; // Hide close button when menu is closed
     submenus.forEach((submenu) => {
       gsap.set(submenu, { x: "-100%", autoAlpha: 0 });
-      submenuVisible.set(submenu, false);  // Reset the visibility state
+      submenuVisibility.set(submenu, false);  // Reset the visibility state
     });
   });
 }
 
 // Event listeners for the menu button and the close button
-menuBtn.addEventListener("click", openMenu);
-closeBtn.addEventListener("click", closeMenu);
+menuButton.addEventListener("click", openMenu);
+closeButton.addEventListener("click", closeMenu);
 
 // Event listeners for the main menu items
 navItems.forEach((item) => {
-  item.addEventListener("click", (e) => {
+  item.addEventListener("click", (event) => {
     const submenu = item.querySelector(".submenu");
-    // Toggle submenu visibility based on its current state
-    if (submenuVisible.get(submenu)) {
+    if (submenuVisibility.get(submenu)) {
       gsap.to(submenu, { x: "-100%", duration: 0.5, autoAlpha: 0 });
-      submenuVisible.set(submenu, false);
+      submenuVisibility.set(submenu, false);
     } else {
       gsap.to(submenu, { x: "0", duration: 0.5, autoAlpha: 1 });
-      submenuVisible.set(submenu, true);
+      submenuVisibility.set(submenu, true);
     }
-    e.stopPropagation();
+    event.stopPropagation();
   });
 });
 
 // Event listener for the overlay to close submenus
 menuOverlay.addEventListener("click", () => {
   submenus.forEach((submenu) => {
-    gsap.to(submenu, { x: "-100%", duration: 0.5, autoAlpha: 0 });
-    submenuVisible.set(submenu, false);  // Reset the visibility state
+    if (submenuVisibility.get(submenu)) {
+      gsap.to(submenu, { x: "-100%", duration: 0.5, autoAlpha: 0 });
+      submenuVisibility.set(submenu, false);  // Reset the visibility state
+    }
   });
 });
