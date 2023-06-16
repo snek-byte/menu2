ff8416ade88e9ccb47aea04a4942ad9188322393
sweston <sarahwestonak@gmail.com>
2023-06-15 19:07:18 -0600

    script.js

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 $(document).ready(function () {
-  $("li").click(function (e) {
-    e.stopPropagation();
-    var subMenu = $(this).children(".sub-menu");
-    if (subMenu.is(":visible")) {
-      // Sublinks disappear to the left using GSAP
-      gsap.to(subMenu, {
-        duration: 0.5,
-        x: -100,
-        autoAlpha: 0,
-        onComplete: function () {
-          subMenu.hide();
-          gsap.set(subMenu, { clearProps: "all" });
+    $("li").click(function (e) {
+        e.stopPropagation();
+
+        var subMenu = $(this).children(".sub-menu");
+        var otherSubMenus = $(this).siblings().children(".sub-menu:visible");
+
+        if (subMenu.is(":visible")) {
+            gsap.to(subMenu, {
+                duration: 0.5,
+                x: -100,
+                autoAlpha: 0,
+                onComplete: function () {
+                    subMenu.hide();
+                    gsap.set(subMenu, { clearProps: "all" });
+                }
+            });
+        } else {
+            subMenu.show();
+            gsap.from(subMenu, { duration: 0.5, x: -100, autoAlpha: 0 });
         }
-      });
-    } else {
-      subMenu.show();
-      // Sublinks appear using GSAP
-      gsap.from(subMenu, { duration: 0.5, x: -100, autoAlpha: 0 });
+
+        if (otherSubMenus.length > 0) {
+            gsap.to(otherSubMenus, {
+                duration: 0.5,
+                x: -100,
+                autoAlpha: 0,
+                onComplete: function () {
+                    otherSubMenus.hide();
+                    gsap.set(otherSubMenus, { clearProps: "all" });
+                }
+            });
+        }
+    });function toggleMenu(id) {
+      var menu = document.getElementById(id);
+      if (menu.className === 'sub-menu') {
+        menu.className = 'sub-menu open';
+      } else {
+        menu.className = 'sub-menu';
+      }
     }
-  });
 });
-function closeNav() {
-    document.getElementById("menu").style.display = "none";
-}
+
+ function toggleFlexbox(id) {
+      var flexbox = document.getElementById(id);
+      if (flexbox.dataset.open === 'true') {
+        flexbox.dataset.open = 'false';
+        flexbox.className = 'flexbox';
+      } else {
+        flexbox.dataset.open = 'true';
+        flexbox.className = 'flexbox open';
+      }
+    }
